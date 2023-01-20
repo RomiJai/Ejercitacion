54988cd1e88f964c93d310c52b0227b0e6f4fcf0
Romy Jai <romina.jaime@icloud.com>
2023-01-20 20:29:42 -0300

    se adjuntaron imagenes y rutas

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-console.log(app);
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+app.listen(4000, () => {
+    console.log('Servidor 4000 ok');
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
